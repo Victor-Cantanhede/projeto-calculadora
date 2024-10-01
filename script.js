377b128769f948045036a943b76ecad93535bc78
Victor-Cantanhede <victorcantanhedblk@gmail.com>
2024-10-01 11:09:36 -0300

    calculadora criada

---
+++ script.js
@@ -0,0 +1,88 @@
+// VARIÁVEL PARA IMPRIMIR EM SCREEN
+let comando = document.getElementById('iprint');
+
+// VARIÁVEL PARA VALOR EM SCREEN
+let inScreen = '';
+
+// VARIÁVEM PARA VALOR EM PARENTESES
+let parenteses = '';
+
+// VARIÁVEL PARA RESULTADO
+let result = '';
+
+
+// FUNÇÃO IMPRIMIR VALOR EM SCREEN
+function addScreen(p) {
+
+    inScreen += p;
+    comando.innerHTML = inScreen;
+}
+
+
+// FUNÇÃO ABRIR OU FECHAR PARENTESES
+function addParenteses() {
+
+    if (parenteses == '') {
+
+        inScreen += '(';
+        comando.innerHTML = inScreen;
+        parenteses = '(';
+
+    } else {
+        
+        inScreen += ')';
+        comando.innerHTML = inScreen;
+        parenteses = '';
+    }
+}
+
+
+// FUNÇÃO PORCENTAGEM
+function porcento() {
+
+    inScreen = Number.parseFloat(eval(inScreen)) / 100;
+
+    if (isNaN(inScreen)) {
+
+        comando.innerHTML = 'Erro! Porcentagem inválida.';
+
+    } else {
+
+        inScreen += '*(';
+        parenteses = '(';
+        comando.innerHTML = inScreen;
+    }    
+}
+
+
+// FUNÇÃO RESULTADO
+function resultado() {
+
+    if (parenteses != '') {
+
+        inScreen += ')';
+        parenteses = '';
+        comando.innerHTML = inScreen;
+    }
+    
+    result = Number.parseFloat(eval(inScreen));
+    comando.innerHTML = result;
+    inScreen = '';
+}
+
+
+// FUNÇÃO LIMPAR TUDO
+function limparTudo() {
+
+    inScreen = '';
+    result = '';
+    parenteses = '';
+    comando.innerHTML = inScreen;
+}
+
+// FUNÇÃO APAGAR ÚLTIMO CARACTER
+function apagar() {
+
+    inScreen = inScreen.slice(0, -1);
+    comando.innerHTML = inScreen;
+}
